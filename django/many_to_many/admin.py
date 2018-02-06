b279a85da1971b6b2c5caed1f6195e1d8ae9c126
chrisaor <pchpch0070@gmail.com>
2018-02-06 11:03:05 +0900

    자기 자신을 MTN field로 갖는 예제 모델 추가

--- django/many_to_many/admin.py
+++ django/many_to_many/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from many_to_many.models import Pizza, Topping, PostLike, Post, User
+from .models import (
+    # basic
+    Pizza, Topping,
+    # intermediate
+    PostLike, Post, User,
+    # self
+    FacebookUser
+)
+
 
 admin.site.register(Pizza)
 admin.site.register(Topping)
 admin.site.register(Post)
 admin.site.register(User)
 admin.site.register(PostLike)
+admin.site.register(FacebookUser)
--- django/many_to_many/models/__init__.py
+++ django/many_to_many/models/__init__.py
@@ -1,2 +1,3 @@
-from django.db import models
-
+from .basic import *
+from .intermediate import *
+from .self import *
