962216659a4411e6b0ddd062b45f6c3fd64a4f91
chrisaor <pchpch0070@gmail.com>
2018-02-06 11:46:49 +0900

    MTM에서 대칭이 False인 모델 예제 추가

--- django/many_to_many/models/self.py
+++ django/many_to_many/models/self.py
@@ -9,4 +9,27 @@ class FacebookUser(models.Model):
     friends = models.ManyToManyField('self')
 
     def __str__(self):
-        return self.name
+        # name이 '이한영'이며
+        # 친구로 '박보영', '아이유'를 가지는 경우
+        # -> 이한영 (친구: 박보영, 아이유)
+        # __str__의 결과가 위처럼 출력될 수 있도록 작성
+        # result = ''
+        # for friend in self.friends.all():
+        #     result += friend.name
+        #     result += ', '
+        # result = result[:-2]
+        # return '{name} (친구: {friends}'.format(
+        #     name=self.name,
+        #     friends=result,
+        # )
+        # list comprehenstion 사용
+        # result = ', '.join([friend.name for friend in self.friends.all()])
+
+        # Manager의 values_list를 사용 <제일 효율적>
+        # DB에서 모든 friends의 'name'필드 값만 가져옴
+        result = ', '.join(self.friends.value_list('name', flat=True))
+
+        return '{name} (친구: {friends})'.format(
+            name=self.name,
+            friends=result,
+        )
