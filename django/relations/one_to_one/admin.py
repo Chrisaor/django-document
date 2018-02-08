40a78336b89fe99bad34c12f14bbb647660064c1
chrisaor <pchpch0070@gmail.com>
2018-02-08 14:59:29 +0900

    relations  관련 내용들을 하나의 패키지에 이동, AppConfig.name재설정

--- django/relations/one_to_one/admin.py
+++ django/relations/one_to_one/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from one_to_one.models import Place, Waiter, Restaurant
+from .models import Place, Waiter, Restaurant
 
 admin.site.register(Place)
 admin.site.register(Restaurant)
